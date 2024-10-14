f5b8f0f11b30eb321d4bd449ecb7c992a06d8f08
DaisyAram <138888608+DaisyAram@users.noreply.github.com>
2024-10-14 11:57:51 +0300

    updated collection page

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { ShopContext } from '../context/Shop'
+import './Pages.css'
 
 const Collection = () => {
+  const { products } = useContext(ShopContext);  //getting data of all products
+  const [showFilter, setShowFilter] = useState(false);
+
   return (
-    <div>
+    <div className='collection-container'>
+      <div className='filters-container'>
+      <p>FILTERS</p>
+      <div className='category-filter'>
+        <p>CATEGORY</p>
+        <div className='checkbox-filter'>
+          <p className='filters'>
+            <input  type='checkbox' value={'Unisex'}/>Unisex
+          </p>
+          <p className='filters'>
+            <input type='checkbox' value={'Ladies'}/>Ladies
+          </p>
+          <p className='filters'>
+            <input type='checkbox' value={'Men'}/>Men
+          </p>
+        </div>
+      </div>
+      <div className='category-filter'>
+        <p>TYPE</p>
+        <div className='checkbox-filter'>
+          <p className='filters'>
+            <input  type='checkbox' value={'Unisex'}/>Unisex
+          </p>
+          <p className='filters'>
+            <input type='checkbox' value={'Ladies'}/>Ladies
+          </p>
+          <p className='filters'>
+            <input type='checkbox' value={'Men'}/>Men
+          </p>
+        </div>
+      </div>
+      </div>
       
     </div>
   );
